4bc3ee151e44324b7b1c106699fc69a95f0e574e
Дмитрий Кабацюра <cauliflb@student.21-school.ru>
2024-01-04 11:05:39 +0300

    Task 6 is done. Setting Gradle

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        // Two.greeting();
+        String reversedText = StringUtils.reverse("Hexlet");
+        System.out.println(reversedText);
     }
 }
